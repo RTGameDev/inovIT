7132d25f9e5e37e64831d4ded895a676a49c7c92
Reshad Tsetskhladze <tsetskhladzereshad@gmail.com>
2025-08-07 11:55:51 +0400

    latest

--- src/assets/pages/WhyUs.jsx
+++ src/assets/pages/WhyUs.jsx
@@ -3,7 +3,7 @@ import './WhyUs.css';
 export default function Services() {
     return (
         <>
-            <h1>იმიტომ რომ ყველაზე მაგრები ვართ!!!</h1>
+            <h1>მრავალ წლიანი გამოცდილება</h1>
         </>
     )
 }
